a0ed4703722cb1609666731fc92e44d490ca0581
eunseonglee <eunseongofficial@gmail.com>
2021-09-25 17:04:17 +0900

    chore: dotenv 적용
    Co-authored-by: eungyeole <eungyeole@naver.com>
    Co-authored-by: ykss <yukyeongsang@gmail.com>

--- imgoing-frontend/babel.config.js
+++ imgoing-frontend/babel.config.js
@@ -1,7 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['babel-plugin-styled-components'],
+    plugins: [
+      'babel-plugin-styled-components',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+    ],
   };
 };
