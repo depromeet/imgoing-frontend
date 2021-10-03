eca3c693b701993523fe98be5f656063a9e82cb0
eunseonglee <eunseongofficial@gmail.com>
2021-10-03 17:49:46 +0900

    [#5] feat: tabcontainer wrapper 추가

--- imgoing-frontend/src/navigation/BottomTabNavigator.tsx
+++ imgoing-frontend/src/navigation/BottomTabNavigator.tsx
@@ -1,43 +1,26 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { BottomTabParamList } from '../types/BottomTabNavigator';
-import { Feather } from "@expo/vector-icons";
+import { NavigationContainer } from '@react-navigation/native';
 
 import SettingScreen from '../screens/SettingsScreen';
 import MainScreen from '../screens/MainScreen';
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
-
-export default function BottomTabNavigator() {
-
+const Tabs = () => {
   return (
-    <BottomTab.Navigator
-      initialRouteName="Main"
-    >
-      <BottomTab.Screen
-        name="Main"
-        component={MainScreen}
-        // options={{
-        //   tabBarIcon: () => <TabBarIcon name="home" color={color/>,
-        //   tabBarLabel: "홈",
-        // }}
-      />
-      <BottomTab.Screen
-        name="Settings"
-        component={SettingScreen}
-        // options={{
-        //   tabBarIcon: ({ color }) => <TabBarIcon name="hash" color={color} />,
-        //   tabBarLabel: "",
-        // }}
-      />
+    <BottomTab.Navigator initialRouteName="Main">
+      <BottomTab.Screen name="Main" component={MainScreen} />
+      <BottomTab.Screen name="Settings" component={SettingScreen} />
     </BottomTab.Navigator>
   );
-}
+};
 
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof Feather>["name"];
-  color: string;
-}) {
-  return <Feather size={20} style={{ marginBottom: -3 }} {...props} />;
+export default function BottomTabNavigator() {
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
