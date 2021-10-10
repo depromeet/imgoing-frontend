c5d11b719480df2b3574a157251dd8ead7b1b3b8
ykss <yukyeongsang@gmail.com>
2021-10-10 22:58:37 +0900

    [#13] feat : 데이터 유무에 따라 MainScreen 설정

--- imgoing-frontend/src/screens/MainScreen.tsx
+++ imgoing-frontend/src/screens/MainScreen.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import HomeLanding from '../components/HomeLanding/HomeLanding';
+import HomeMain from '../components/HomeMain/HomeMain';
+import PLANS from '../mock/plan.mock';
 
 const MainScreen = () => {
-  return (
-    <Wrapper>
-      <HomeLanding />
-    </Wrapper>
-  );
+  return <Wrapper>{PLANS ? <HomeMain /> : <HomeLanding />}</Wrapper>;
 };
 
 export default MainScreen;
