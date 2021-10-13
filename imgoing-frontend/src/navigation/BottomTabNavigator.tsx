fa83070a33d0127068feef42202d7d38d5323471
Eungyeol <eungyeole@naver.com>
2021-10-13 22:22:38 +0900

    [#24] refactor: Navigaton에 Stack 추가

--- imgoing-frontend/src/navigation/BottomTabNavigator.tsx
+++ imgoing-frontend/src/navigation/BottomTabNavigator.tsx
@@ -2,11 +2,36 @@ import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { BottomTabParamList } from '../types/BottomTabNavigator';
 import { NavigationContainer } from '@react-navigation/native';
-
 import SettingScreen from '../screens/SettingsScreen';
 import MainScreen from '../screens/MainScreen';
+import EditScreen from '../screens/EditScreen';
+import { createStackNavigator } from '@react-navigation/stack'
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+const Stack = createStackNavigator();
+
+const Stacks = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }}/>
+      <Stack.Screen 
+        name="Edit" 
+        component={EditScreen} 
+        options={{
+          title: "편집하기",
+          headerShown: true,
+          headerTitleAlign: "left",
+          headerLeft: () => (
+            <>{/*SVG 로드 설정 후 제작*/}</>
+          ),
+          headerStyle: {
+            shadowColor: 'transparent'
+          }
+        }} 
+      />
+    </Stack.Navigator>
+  )
+}
 
 const Tabs = () => {
   return (
@@ -24,7 +49,7 @@ const Tabs = () => {
 export default function BottomTabNavigator() {
   return (
     <NavigationContainer>
-      <Tabs />
+      <Stacks />
     </NavigationContainer>
   );
 }
