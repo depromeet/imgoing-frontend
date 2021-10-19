0c14254497b51f1d29da614b1dbcd5eedd1791af
eunseonglee <eunseongofficial@gmail.com>
2021-10-20 01:03:24 +0900

    [#15] feat: PlanAdd 화면 구성

---
+++ imgoing-frontend/src/components/PlanAdd/ProgressBar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const Wrapper = styled.View`
+  height: 150px;
+  background-color: tomato;
+`;
+
+const ProgressBar = () => {
+  return (
+    <Wrapper>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+      <Text>ProgresBar</Text>
+    </Wrapper>
+  );
+};
+
+export default ProgressBar;
