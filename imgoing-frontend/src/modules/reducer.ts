a9e62bd746ce936b3aebde7654124d1dbf389cfe
eunseonglee <eunseongofficial@gmail.com>
2021-10-23 02:13:52 +0900

    [#55] feat: addPlan 리듀서 등록

--- imgoing-frontend/src/modules/reducer.ts
+++ imgoing-frontend/src/modules/reducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import users from 'modules/slices/user';
 import modal from 'modules/slices/modal';
+import stepOfAddingPlan from './slices/stepOfAddingPlan';
 
 const reducer = combineReducers({
   users,
   modal,
+  stepOfAddingPlan,
 });
 
 export type ReducerType = ReturnType<typeof reducer>;
