701792b7d8f98a49d17f44762cd8c7b2a63bb200
eunseonglee <eunseongofficial@gmail.com>
2021-10-23 02:07:45 +0900

    [#54] feat: plan 관련 타입 추가

--- imgoing-frontend/src/types/index.ts
+++ imgoing-frontend/src/types/index.ts
@@ -1,12 +1,3 @@
-export interface Plan {
-  name: string;
-  arrival_at: string;
-  destination: Destination;
-  memo: string;
-  items: string;
-  tasks: Task[];
-}
-
 export interface Task {
   name: string;
   duration: number;
@@ -19,3 +10,71 @@ export interface Destination {
   dest_lat: number;
   dest_lng: number;
 }
+
+export interface Plan {
+  name: string;
+  arrival_at: string;
+  destination: Destination;
+  memo: string;
+  items: string;
+  tasks: Task[];
+}
+
+type setTitleType = {
+  title: string;
+};
+type setDepartureType = {
+  name: string;
+  address: string;
+  coordinate: {
+    lat: number | undefined;
+    lng: number | undefined;
+  };
+};
+type setArrivalType = {
+  name: string;
+  address: string;
+  coordinate: {
+    lat: number | undefined;
+    lng: number | undefined;
+  };
+};
+type setArrivalTimeType = {
+  date: string;
+};
+type setItemType = {
+  items: string;
+};
+type setDetailsType = {
+  contents: string;
+};
+type setTaskType = {
+  tasks: Task[];
+};
+export interface AddingPlanSteps {
+  setTitle: setTitleType;
+  setDeparture: setDepartureType;
+  setArrival: setArrivalType;
+  setArrivalTime: setArrivalTimeType;
+  setItem: setItemType;
+  setDetails: setDetailsType;
+  setTask: setTaskType;
+}
+export interface AddingPlanStepsType {
+  setTitle: undefined;
+  setDeparture: undefined;
+  setArrival: undefined;
+  setArrivalTime: undefined;
+  setItem: undefined;
+  setDetails: undefined;
+  setTask: undefined;
+}
+
+export type PlanStepInfo = {
+  [key in keyof AddingPlanStepsType]: {
+    nextStep: keyof AddingPlanStepsType | null;
+    prevStep: keyof AddingPlanStepsType | null;
+    title: string;
+    percentage: number;
+  };
+};
