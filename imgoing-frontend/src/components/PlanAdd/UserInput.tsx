1bac4470074bca287b5878cf0eeb67871809bb9a
eunseonglee <eunseongofficial@gmail.com>
2021-10-24 02:41:46 +0900

    [#55] feat: 단순 input 값 가져와서 addingPlanState 변경하는 로직 추가

--- imgoing-frontend/src/components/PlanAdd/UserInput.tsx
+++ imgoing-frontend/src/components/PlanAdd/UserInput.tsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
+import React, { Dispatch, useState } from 'react';
 import styled from 'styled-components/native';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { NavigationScreenProp } from 'react-navigation';
 
 import { icon_openRight, icon_plus } from 'assets/svg';
 import { AddingPlanStepsType } from 'types/index';
 import { PLAN_STEP_TITLES } from 'constant/plan';
 import Input from 'components/common/Input';
 import RectangleButton from 'components/common/RectangleButton';
-import { resetStep } from 'modules/slices/stepOfAddingPlan';
+
+interface UserInputProps {
+  navigation: NavigationScreenProp<any, any>;
+  setInputText: (text: string) => void;
+}
 
 const Wrapper = styled.View`
   flex: 1;
@@ -19,8 +23,14 @@ const EditInput = styled(Input)`
   margin-bottom: 40px;
 `;
 
-const Step1 = () => {
-  return <EditInput title='스케줄 이름을 입력해 주세요' placeholder='스케줄 이름 입력하기' />;
+const Step1 = ({ setInputText }: { setInputText: (text: string) => void }) => {
+  return (
+    <EditInput
+      title='스케줄 이름을 입력해 주세요'
+      placeholder='스케줄 이름 입력하기'
+      onChangeText={setInputText}
+    />
+  );
 };
 
 const Step2 = () => {
@@ -41,30 +51,38 @@ const Step4 = () => {
   return <EditInput title='도착 시간을 입력해 주세요' placeholder='날짜 / 시간 입력하기' />;
 };
 
-const Step5 = () => {
-  return <EditInput title='외출 시 필요한 물품이 있나요' />;
+const Step5 = ({ setInputText }: { setInputText: (text: string) => void }) => {
+  return (
+    <EditInput
+      title='외출 시 필요한 물품이 있나요'
+      placeholder='필요한 물품 입력하기'
+      onChangeText={setInputText}
+    />
+  );
 };
 
-const Step6 = () => {
-  return <EditInput title='일정에 대한 상세 내용을 알려주세요' />;
+const Step6 = ({ setInputText }: { setInputText: (text: string) => void }) => {
+  return (
+    <EditInput
+      title='일정에 대한 상세 내용을 알려주세요'
+      placeholder='상세 내용 입력하기'
+      onChangeText={setInputText}
+    />
+  );
 };
 
 const Step7 = () => {
   return <RectangleButton leftIcon={icon_plus}>등록해 주세요</RectangleButton>;
 };
 
-const UserInput = () => {
-  const stepOfAddingPlan = useSelector((state) => state.stepOfAddingPlan);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (!stepOfAddingPlan.step) dispatch(resetStep());
-  }, [stepOfAddingPlan.step]);
+const UserInput = (props: UserInputProps) => {
+  const { navigation, setInputText } = props;
+  const step = useSelector((state) => state.stepOfAddingPlan.step);
 
   const currentStep = (step: keyof AddingPlanStepsType | null) => {
     switch (step) {
       case PLAN_STEP_TITLES.SET_TITLE:
-        return <Step1 />;
+        return <Step1 setInputText={setInputText} />;
       case PLAN_STEP_TITLES.SET_DEPARTURE:
         return <Step2 />;
       case PLAN_STEP_TITLES.SET_ARRIVAL:
@@ -72,17 +90,18 @@ const UserInput = () => {
       case PLAN_STEP_TITLES.SET_ARRIVALTIME:
         return <Step4 />;
       case PLAN_STEP_TITLES.SET_ITEM:
-        return <Step5 />;
+        return <Step5 setInputText={setInputText} />;
       case PLAN_STEP_TITLES.SET_DETAILS:
-        return <Step6 />;
+        return <Step6 setInputText={setInputText} />;
       case PLAN_STEP_TITLES.SET_TASK:
         return <Step7 />;
       default:
-        return <Text>done</Text>;
+        navigation.goBack();
+        break;
     }
   };
 
-  return <Wrapper>{currentStep(stepOfAddingPlan.step)}</Wrapper>;
+  return <Wrapper>{currentStep(step)}</Wrapper>;
 };
 
 export default UserInput;
--- imgoing-frontend/src/screens/PlanAddScreen.tsx
+++ imgoing-frontend/src/screens/PlanAddScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
 import { useDispatch } from 'react-redux';
 
@@ -7,7 +7,9 @@ import BottomButtonLayout from 'layouts/BottomButtonLayout';
 import UserInput from 'components/PlanAdd/UserInput';
 import { resetStep, setStep } from 'modules/slices/stepOfAddingPlan';
 import { NavigationScreenProp } from 'react-navigation';
-import { AddPlanContentsType } from 'types/index';
+import { AddingPlanStepsType, AddPlanContentsType } from 'types/index';
+import store from 'modules/store';
+import { PLAN_STEP_TITLES } from 'constant/plan';
 
 interface PlanAddScreenProps {
   navigation: NavigationScreenProp<any, any>;
@@ -19,19 +21,71 @@ const Wrapper = styled.View`
 `;
 
 const PlanAddScreen = (props: PlanAddScreenProps) => {
-  const contents = useRef<AddPlanContentsType | null>(null);
-  const dispatch = useDispatch();
+  const contents = {} as { [key: string]: AddPlanContentsType };
+  const [inputText, setInputText] = useState<string>('');
 
-  const setContents = (newContent: AddPlanContentsType) => {
-    contents.current = newContent;
-  };
+  const dispatch = useDispatch();
+  const [step, setStepState] = useState<keyof AddingPlanStepsType | null>(
+    store.getState().stepOfAddingPlan.step,
+  );
 
   const onPress = () => {
-    if (!contents.current) return;
-    dispatch(setStep({ type: 'next', userInput: contents.current }));
+    switch (step) {
+      case PLAN_STEP_TITLES.SET_TITLE:
+        contents.setTitle = {
+          title: inputText,
+        };
+        break;
+      case PLAN_STEP_TITLES.SET_DEPARTURE:
+        contents.setDeparture = {
+          name: '집',
+          address: '강북',
+          coordinate: {
+            lat: -1,
+            lng: -1,
+          },
+        };
+        break;
+      case PLAN_STEP_TITLES.SET_ARRIVAL:
+        contents.setArrival = {
+          name: '식당',
+          address: '강남',
+          coordinate: {
+            lat: 0,
+            lng: 0,
+          },
+        };
+        break;
+      case PLAN_STEP_TITLES.SET_ARRIVALTIME:
+        contents.setArrivalTime = {
+          date: '2021-09-09 12:12:12',
+        };
+        break;
+      case PLAN_STEP_TITLES.SET_ITEM:
+        contents.setItem = {
+          title: inputText,
+        };
+        break;
+      case PLAN_STEP_TITLES.SET_DETAILS:
+        contents.setDetails = {
+          items: inputText,
+        };
+        break;
+      case PLAN_STEP_TITLES.SET_TASK:
+        contents.setTask = {
+          tasks: [],
+        };
+        break;
+      default:
+        break;
+    }
+    dispatch(setStep({ type: 'next', userInput: contents }));
   };
 
   useEffect(() => {
+    store.subscribe(() => {
+      setStepState(store.getState().stepOfAddingPlan.step);
+    });
     dispatch(resetStep());
   }, []);
 
@@ -39,7 +93,7 @@ const PlanAddScreen = (props: PlanAddScreenProps) => {
     <Wrapper>
       <BottomButtonLayout text='다음' onPress={onPress}>
         <ProgressBar />
-        <UserInput navigation={props.navigation} setContents={setContents} />
+        <UserInput navigation={props.navigation} setInputText={setInputText} />
       </BottomButtonLayout>
     </Wrapper>
   );
