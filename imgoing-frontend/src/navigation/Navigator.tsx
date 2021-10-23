8751159ad0bf4ffc50f992a66ff5ed507e67e67f
eunseonglee <eunseongofficial@gmail.com>
2021-10-24 00:53:35 +0900

    [#55] feat: stepState에 따른 헤더 버튼 동작 추가

--- imgoing-frontend/src/navigation/Navigator.tsx
+++ imgoing-frontend/src/navigation/Navigator.tsx
@@ -1,49 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { SvgXml } from 'react-native-svg';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 import { icon_arrowLeft, icon_close } from 'assets/svg';
 import { NavigatorParamList } from 'types/Route';
+import { AddingPlanStepsType } from 'types/index';
 import { colors } from 'constant/index';
+import { firstStep } from 'constant/plan';
+import store from 'modules/store';
+import { setStep, resetStep } from 'modules/slices/stepOfAddingPlan';
 import PlanEditScreen from 'screens/PlanEditScreen';
 import PlanAddScreen from 'screens/PlanAddScreen';
 import LoginScreen from 'screens/LoginScreen';
 import AuthLoadingScreen from 'screens/AuthLoadingScreen';
 import MainBottomTab from 'navigation/MainBottomTab';
-import { resetStep } from 'modules/slices/stepOfAddingPlan';
 import ModalContainer from 'components/Modal';
 
 const Stack = createStackNavigator<NavigatorParamList>();
 
 const Stacks = () => {
-  // const step = useSelector(
-  //   (state) => state.stepOfAddingPlan.step
-  // );
   const dispatch = useDispatch();
+  const [step, setStepState] = useState<keyof AddingPlanStepsType | null>(
+    store.getState().stepOfAddingPlan.step,
+  );
 
-  const onHeaderRightPress = (navigation: any) => {
-    return (
-      <SvgXml
-        xml={icon_close}
-        style={{ marginRight: 16 }}
-        fill={colors.black}
-        onPressOut={() => navigation.goBack()}
-      />
-    );
-  };
-  const onHeaderLeftPress = (navigation: any) => {
-    return (
-      <SvgXml
-        xml={icon_arrowLeft}
-        style={{ marginLeft: 16 }}
-        fill={colors.black}
-        onPressOut={() => navigation.goBack()}
-      />
-    );
-  };
+  useEffect(() => {
+    store.subscribe(() => {
+      setStepState(store.getState().stepOfAddingPlan.step);
+    });
+  }, []);
 
   return (
     <Stack.Navigator initialRouteName='AuthLoadingScreen'>
@@ -80,8 +68,40 @@ const Stacks = () => {
           title: '스케줄 등록',
           headerShown: true,
           headerTitleAlign: 'left',
-          headerRight: () => onHeaderRightPress(navigation),
-          headerLeft: () => onHeaderLeftPress(navigation),
+          headerRight: () => {
+            useEffect(() => {
+              dispatch(resetStep());
+            }, []);
+
+            return (
+              <SvgXml
+                xml={icon_close}
+                style={{ marginRight: 16 }}
+                fill={colors.black}
+                onPressOut={() => navigation.goBack()}
+              />
+            );
+          },
+          headerLeft: () => {
+            useEffect(() => {
+              if (step === (firstStep as string)) {
+                dispatch(resetStep());
+              }
+            }, []);
+
+            return (
+              <SvgXml
+                xml={icon_arrowLeft}
+                style={{ marginLeft: 16 }}
+                fill={colors.black}
+                onPressOut={() => {
+                  step === (firstStep as string)
+                    ? navigation.goBack()
+                    : dispatch(setStep({ type: 'prev', userInput: { title: 'title' } }));
+                }}
+              />
+            );
+          },
           headerStyle: {
             height: getStatusBarHeight() + 54,
             shadowColor: 'transparent',
