bf17eff2961bb26d2c44ba30cdd41d064fd6151c
ykss <yukyeongsang@gmail.com>
2021-10-25 01:10:34 +0900

    [#68] style : 버튼 위치 수정 및 컴포넌트 반응형으로 수정 반영

--- imgoing-frontend/src/components/HomeMain/index.tsx
+++ imgoing-frontend/src/components/HomeMain/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavigationScreenProp } from 'react-navigation';
+import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
 
 import TimeReminder from './TimeReminder';
 import PlanList from './PlanList';
@@ -9,17 +11,25 @@ interface HomeMainProps {
   navigation: NavigationScreenProp<any, any>;
 }
 
+const Wrapper = styled.View`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10%;
+`;
+
 const HomeMain = (props: HomeMainProps) => {
   const { navigation } = props;
 
   return (
-    <>
+    <Wrapper>
       <TimeReminder />
       <PlanList />
       {/* 시간 지나면 바뀌도록 애니메이션 넣어야 함 */}
       {/* 현재는 full prop 넣으면 전체 표시하도록 하였음. */}
       <AddPlanButton onPress={() => navigation.navigate('PlanAdd')}>일정 등록하기</AddPlanButton>
-    </>
+    </Wrapper>
   );
 };
 
