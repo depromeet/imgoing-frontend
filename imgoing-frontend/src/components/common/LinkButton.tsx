376c8b014a9547d865fae1a675bc39dfc77ac2b4
eunseonglee <eunseongofficial@gmail.com>
2021-10-26 02:12:24 +0900

    [#70] fix: LinkButton의 padding값으로 인한 잘못된 터치 오류 해결

--- imgoing-frontend/src/components/common/LinkButton.tsx
+++ imgoing-frontend/src/components/common/LinkButton.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
-import { TouchableOpacityProps } from 'react-native';
-import styled from 'styled-components/native';
+import { TextProps } from 'react-native';
 
 import { SubheadlineTypo } from 'components/typography';
 
-interface ButtonProps extends TouchableOpacityProps {
+interface ButtonProps extends TextProps {
   children: React.ReactNode;
 }
 
-const StyledButton = styled.TouchableOpacity`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-`;
-
 const LinkButton = (props: ButtonProps) => {
   return (
-    <StyledButton {...props} activeOpacity={0.6}>
-      <SubheadlineTypo bold color={'black'} style={{ textDecorationLine: 'underline' }}>
-        {props.children}
-      </SubheadlineTypo>
-    </StyledButton>
+    <SubheadlineTypo {...props} bold color={'black'} style={{ textDecorationLine: 'underline' }}>
+      {props.children}
+    </SubheadlineTypo>
   );
 };
 
