7848817da52864e50111e6edaef1f94dc6749040
eunseonglee <eunseongofficial@gmail.com>
2021-10-27 04:40:33 +0900

    [#88] feat: 선택된 일시 store에 저장

--- imgoing-frontend/src/components/Modal/DateWheelPickerModal.tsx
+++ imgoing-frontend/src/components/Modal/DateWheelPickerModal.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components/native';
 
 import WheelPicker from 'components/common/WheelPicker';
 import { removeModal } from 'modules/slices/modal';
 import RoundBottomModalLayout from 'layouts/RoundBottomModalLayout';
+import { setStep } from 'modules/slices/stepOfAddingPlan';
 
 const PickerContainer = styled.View`
   display: flex;
@@ -17,55 +18,91 @@ const Picker = styled.View`
   padding: 0 16px;
 `;
 
-const getDateFromToday = (year = 0, month = 0, date = 0): number[] => {
+const day = ['일', '월', '화', '수', '목', '금', '토'];
+
+const getDateFromToday = (year = 0, month = 0, date = 0): string[] => {
   const [todayYear, todayMonth, todayDate] = new Date()
     .toLocaleDateString()
     .replaceAll(' ', '')
-    .split('.')
-    .map((i) => +i);
+    .split('.');
 
   if (year === 0 && month === 0 && date === 0) return [todayYear, todayMonth, todayDate];
 
-  return new Date(todayYear + year, todayMonth - 1 + month, todayDate + date)
-    .toLocaleDateString()
-    .replaceAll(' ', '')
-    .split('.')
-    .map((i) => +i);
+  const newDate = new Date(+todayYear + year, +todayMonth - 1 + month, +todayDate + date);
+  return [
+    ...newDate.toLocaleDateString().replaceAll(' ', '').split('.').slice(0, 3),
+    day[newDate.getDay()],
+  ];
 };
 
-const getDateList = () => {
+const getDateList = (): string[] => {
   const list = ['오늘', '내일'];
   for (let i = 0; i < 60; i++) {
     const dateItem = getDateFromToday(0, 0, i + 2);
-    list.push(`${dateItem[1]}월 ${dateItem[2]}일`);
+    list.push(`${dateItem[1]}월 ${dateItem[2]}일 (${dateItem[3]})`);
   }
   return list;
 };
 
-const getTimeList = () => {
-  const duration = 30;
+const getTimeList = (duration = 30): string[] => {
   const list = [];
-
   for (let i = 0; i < 24; i++) {
     for (let j = 0; j < 60 / duration; j++) {
-      list.push(`${i}시 ${(j * duration).toString().padEnd(2, '0')}분`);
+      list.push(`${i}시 ${(j * duration).toString().padEnd(2, '0')}분`);
     }
   }
   return list;
 };
 
+const getArrivalDateTime = (timeString: string, date: string[]) => {
+  const [hour, , miniutes, _]: string[] = timeString
+    .replaceAll(' ', '')
+    .split(/(시|분)/g)
+    .map((i) => i.toString());
+  console.log(timeString, timeString.replaceAll(' ', '').split(/(시|분)/g));
+
+  return `${date[0]}-${date[1]}-${date[2]} ${hour.padEnd(2, '0')}:${miniutes.padEnd(2, '0')}:00`;
+};
+
 const DateWheelPickerModal = () => {
   const dispatch = useDispatch();
+  const selectedDateIndex = useRef<number>(1);
+  const selectedTime = useRef<string>(getTimeList()[24]);
+
+  const onDateIndexChange = (value: string | number, index: number) => {
+    selectedDateIndex.current = index;
+  };
+
+  const onTimeChange = (value: string | number) => {
+    selectedTime.current = value as string;
+    console.log(selectedTime.current);
+  };
+
+  const onPress = () => {
+    const arrivalDateTime = getArrivalDateTime(
+      selectedTime.current,
+      getDateFromToday(0, 0, selectedDateIndex.current),
+    );
+
+    dispatch(
+      setStep({
+        type: null,
+        userInput: {
+          arrivalDateTime: arrivalDateTime,
+        },
+      }),
+    );
+    dispatch(removeModal());
+  };
 
   return (
-    <RoundBottomModalLayout
-      button={{ onPress: () => dispatch(removeModal()), buttonName: '선택 완료' }}>
+    <RoundBottomModalLayout button={{ onPress, buttonName: '선택 완료' }}>
       <PickerContainer>
         <Picker>
-          <WheelPicker dataSource={getDateList()} />
+          <WheelPicker dataSource={getDateList()} onValueChange={onDateIndexChange} />
         </Picker>
         <Picker>
-          <WheelPicker dataSource={getTimeList()} />
+          <WheelPicker dataSource={getTimeList()} onValueChange={onTimeChange} selectedIndex={24} />
         </Picker>
       </PickerContainer>
     </RoundBottomModalLayout>
