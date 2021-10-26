b670242ab9c71abd2fc8d14e93ec72c17040a3eb
eunseonglee <eunseongofficial@gmail.com>
2021-10-26 19:17:51 +0900

    [#70] feat: 북마크 불러오기 기능 추가
    불러온 후 선택 완료하면 준비 항목 등록에 리스트업

--- imgoing-frontend/src/components/Modal/LoadBookmark.tsx
+++ imgoing-frontend/src/components/Modal/LoadBookmark.tsx
@@ -1,31 +1,51 @@
 import React, { useState } from 'react';
 import { ScrollView } from 'react-native';
 import { useDispatch } from 'react-redux';
-import styled from 'styled-components/native';
 
 import { removeModal } from 'modules/slices/modal';
 import RoundBottomModalLayout from 'layouts/RoundBottomModalLayout';
-import Task from 'components/Task';
 import { tasks } from 'mocks/plan.mock';
+import { setStep } from 'modules/slices/stepOfAddingPlan';
+import store from 'modules/store';
+import BookmarkItem from 'components/BookmarkItem';
 
 const DateWheelPickerModal = () => {
   const dispatch = useDispatch();
+  const [selectedItems, setSelectedItem] = useState<number[]>([]);
+  const tasksState = store.getState().stepOfAddingPlan.userInputs.tasks;
 
   return (
     <RoundBottomModalLayout
       title={'불러오기'}
-      button={{ onPress: () => dispatch(removeModal()), buttonName: '선택 완료' }}>
+      button={{
+        onPress: () => {
+          dispatch(
+            setStep({
+              type: null,
+              userInput: {
+                tasks: tasksState
+                  ? [...tasksState, ...selectedItems.map((id) => tasks[id])]
+                  : [...selectedItems.map((id) => tasks[id])],
+              },
+            }),
+          );
+          dispatch(removeModal());
+        },
+        buttonName: '선택 완료',
+      }}>
       <ScrollView style={{ height: 350 }}>
         {tasks
           .filter((task) => task.isBookmarked)
           .map((task, idx) => (
-            <Task
-              // key가 task의 id값이 들어가야 하는데 일단 mock데이터는 건들지 않고 index로 진행
+            <BookmarkItem
+              // TODO: key가 task의 id값이 들어가야 하는데 일단 mock데이터는 건들지 않고 index로 진행
               key={idx}
-              minutes={task.duration}
-              title={task.name}
-              defaultNotification={task.notification}
-              isBookmarked={task.isBookmarked}
+              id={idx}
+              duration={task.duration}
+              name={task.name}
+              notification={task.notification}
+              setSelectedItem={setSelectedItem}
+              selectedItems={selectedItems}
             />
           ))}
       </ScrollView>
