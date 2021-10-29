a06e2ad05b42f1ad3a69d23badcfeec7a6d5b643
eunseonglee <eunseongofficial@gmail.com>
2021-10-29 19:20:18 +0900

    hotfix: baseurl에서 localhost 삭제

--- imgoing-frontend/src/environments.ts
+++ imgoing-frontend/src/environments.ts
@@ -1,7 +1,7 @@
 import { BASE_URL, KAKAO_AUTH_REST_API_ID, AUTH_REDIRECT_URL, DUMMY_TOKNE } from '@env';
 
 const ENV = {
-  apiUrl: __DEV__ ? 'localhost' : BASE_URL,
+  apiUrl: BASE_URL,
   kakaoClientId: KAKAO_AUTH_REST_API_ID,
   redirectUrl: AUTH_REDIRECT_URL,
   accessToken: DUMMY_TOKNE,
