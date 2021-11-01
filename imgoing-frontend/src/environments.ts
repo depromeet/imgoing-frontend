fd2c269ec71150aa415e191644f4cd7f20cf53c7
eunseonglee <eunseongofficial@gmail.com>
2021-11-02 00:47:05 +0900

    [#95] chore: dummy 환경 변수 삭제

--- imgoing-frontend/src/environments.ts
+++ imgoing-frontend/src/environments.ts
@@ -1,10 +1,9 @@
-import { BASE_URL, KAKAO_AUTH_REST_API_ID, AUTH_REDIRECT_URL, DUMMY_TOKNE } from '@env';
+import { BASE_URL, KAKAO_AUTH_REST_API_ID, AUTH_REDIRECT_URL } from '@env';
 
 const ENV = {
   apiUrl: BASE_URL,
   kakaoClientId: KAKAO_AUTH_REST_API_ID,
   redirectUrl: AUTH_REDIRECT_URL,
-  accessToken: DUMMY_TOKNE,
 };
 
 export default ENV;
