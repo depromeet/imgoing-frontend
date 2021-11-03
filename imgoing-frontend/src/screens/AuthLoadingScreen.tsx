c01f378e83480a736562a1f107960b3f32e4d69a
eunseonglee <eunseongofficial@gmail.com>
2021-11-04 00:51:13 +0900

    [#114] feat: AuthLoading에서 북마크 api 조회

--- imgoing-frontend/src/screens/AuthLoadingScreen.tsx
+++ imgoing-frontend/src/screens/AuthLoadingScreen.tsx
@@ -1,21 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text } from 'react-native';
+import { useDispatch } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import styled from 'styled-components/native';
 import { useNavigation } from '@react-navigation/native';
+
 import { NavigatorParams } from 'types/Route';
+import { getBookmarkList } from 'modules/thunks/bookmark';
+
+const Wrapper = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
 
 const AuthLoadingScreen = () => {
+  const dispatch = useDispatch();
   const navigation = useNavigation<NavigatorParams>();
+  const [accessToken, setAccessToken] = useState<string | null>();
 
   const getUserToken = async () => {
     const token = await AsyncStorage.getItem('accessToken');
-    navigation.navigate(token ? 'Main' : 'Login');
+    setAccessToken(token);
   };
 
   useEffect(() => {
     getUserToken();
   }, []);
+
+  useEffect(() => {
+    if (accessToken === undefined) return;
+
+    if (accessToken === null) {
+      navigation.navigate('Login');
+    } else {
+      // TODO : plan 조회
+      dispatch(getBookmarkList());
+      navigation.navigate('Main');
+    }
+  }, [accessToken]);
 
   return (
     <Wrapper>
@@ -25,9 +48,3 @@ const AuthLoadingScreen = () => {
 };
 
 export default AuthLoadingScreen;
-
-const Wrapper = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
-`;
