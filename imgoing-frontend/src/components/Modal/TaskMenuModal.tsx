84a0deea86782ee12da398c4c490772a946bdf54
eunseonglee <eunseongofficial@gmail.com>
2021-11-04 03:51:21 +0900

    [#92] feat: 준비항목 북마크 등록 로직 추가

--- imgoing-frontend/src/components/Modal/TaskMenuModal.tsx
+++ imgoing-frontend/src/components/Modal/TaskMenuModal.tsx
@@ -4,8 +4,10 @@ import styled, { css } from 'styled-components/native';
 
 import RoundBottomModalLayout from 'layouts/RoundBottomModalLayout';
 import { CalloutTypo } from 'components/typography';
+import store from 'modules/store';
 import { removeModal } from 'modules/slices/modal';
 import { removeTask } from 'modules/slices/stepOfAddingPlan';
+import { createBookmark, deleteBookmark } from 'modules/thunks/bookmark';
 
 interface ModalButtonProps {
   first?: boolean;
@@ -37,25 +39,39 @@ const ModalButton = styled.TouchableOpacity<Pick<ModalButtonProps, 'first'>>`
 const TaskMenuModal = () => {
   const dispatch = useDispatch();
   const identify = useSelector((state) => state.identify);
-
-  const addBookmark = () => {
+  const tasks = store.getState().stepOfAddingPlan.userInputs.tasks || null;
+  if (identify?.type !== 'task' || !tasks) {
     dispatch(removeModal());
+    // TODO : 없는 준비 항목이에요 - 토스트 메세지
+    console.log('없는 준비 항목이에요');
+    return;
+  }
+
+  const task = tasks.find((task) => task.id === identify.id);
+  if (!task) {
+    dispatch(removeModal());
+    // TODO : 없는 준비 항목이에요 - 토스트 메세지
+    console.log('없는 준비 항목이에요');
+    return;
+  }
+
+  const toggleBookmark = () => {
+    dispatch(removeModal());
+    dispatch(!task.isBookmarked ? createBookmark(task) : deleteBookmark(task.id));
   };
 
   const deleteTask = () => {
     dispatch(removeModal());
-    if (identify?.type !== 'task') {
-      // TODO : 삭제에 실패하였습니다 - 토스트 메세지
-      return;
-    }
     dispatch(removeTask(identify.id));
   };
 
   return (
     <RoundBottomModalLayout>
-      <ModalButton onPress={addBookmark}>
-        <CalloutTypo color={'black'}>북마크로 등록하기</CalloutTypo>
-      </ModalButton>
+      {!task.isBookmarked && (
+        <ModalButton onPress={toggleBookmark}>
+          <CalloutTypo color={'black'}>북마크로 등록하기</CalloutTypo>
+        </ModalButton>
+      )}
       <ModalButton onPress={deleteTask}>
         <CalloutTypo color={'black'}>삭제하기</CalloutTypo>
       </ModalButton>
