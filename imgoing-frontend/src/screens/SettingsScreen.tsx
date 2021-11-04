585f3e870fc10fc78a4bff274c5360da2e9356a8
eunseonglee <eunseongofficial@gmail.com>
2021-11-05 03:49:04 +0900

    [#125] feat: 로그아웃 기능 추가

--- imgoing-frontend/src/screens/SettingsScreen.tsx
+++ imgoing-frontend/src/screens/SettingsScreen.tsx
@@ -1,19 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import RoundButton from 'components/common/RoundButton';
 import React from 'react';
-import { Text } from 'react-native';
 import styled from 'styled-components/native';
-
-const SettingScreen = () => {
-  return (
-    <Wrapper>
-      <Text>Settings</Text>
-    </Wrapper>
-  );
-};
-
-export default SettingScreen;
+import { NavigatorParams } from 'types/Route';
 
 const Wrapper = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
 `;
+
+const SettingScreen = () => {
+  const navigation = useNavigation<NavigatorParams>();
+
+  const onPress = async () => {
+    await AsyncStorage.removeItem('accesstoken');
+    navigation.navigate('Login');
+  };
+
+  return (
+    <Wrapper>
+      <RoundButton style={{ width: '70%', maxWidth: 300 }} onPress={onPress}>
+        로그아웃
+      </RoundButton>
+    </Wrapper>
+  );
+};
+
+export default SettingScreen;
