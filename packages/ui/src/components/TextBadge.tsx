ba05dc05446d37240b4740486429e1c70dce1579
Eungyeol <eungyeole@naver.com>
2021-11-24 01:56:58 +0900

    refactor: TextBadge 모듈 import 순서 변경

--- packages/ui/src/components/TextBadge.tsx
+++ packages/ui/src/components/TextBadge.tsx
@@ -1,7 +1,7 @@
+import { Text } from './Text';
 import { ColorNames, colors } from 'design-token';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Text } from './Text';
 
 interface TextBadgeProps {
   text: string;
