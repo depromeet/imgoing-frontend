dc886f4759fc7c74e960a13d4261c4b2eb7a90c4
ykss <yukyeongsang@gmail.com>
2021-12-11 01:54:52 +0900

    [#192] feat : MyPlanScreen 화면 구성

--- packages/app/src/screens/MyPlanScreen.tsx
+++ packages/app/src/screens/MyPlanScreen.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { SvgXml } from 'react-native-svg';
 
-import { Text } from 'ui';
+import PlanStatistics from 'components/myPlan/PlanStatistics';
+import { colors } from 'design-token';
+import { icon_arrowRight } from 'icons';
+import { ListContainer, Stack, Text } from 'ui';
 
 const MyPlanScreen = () => {
+  const [toggleExpand, setToggleExpand] = useState(false);
   return (
-    <View>
-      <Text>MyPlanScreen</Text>
-    </View>
+    <ScrollView style={styles.wrapper}>
+      <Stack>
+        <Text fontType='BOLD_24'>어머나..🤢</Text>
+        <Text fontType='BOLD_24'>이달은 2번 지각했어요</Text>
+      </Stack>
+      <PlanStatistics
+        toggleExpand={toggleExpand}
+        onPress={() => {
+          setToggleExpand(!toggleExpand);
+        }}
+      />
+      <View style={styles.gap} />
+      <Stack title='일정 관리'>
+        <ListContainer text='루틴 관리'>
+          <SvgXml xml={icon_arrowRight} />
+        </ListContainer>
+        <ListContainer text='지난 일정 보기'>
+          <SvgXml xml={icon_arrowRight} />
+        </ListContainer>
+      </Stack>
+      <Stack title='장소 관리'>
+        <ListContainer text='즐겨찾는 장소'>
+          <SvgXml xml={icon_arrowRight} />
+        </ListContainer>
+      </Stack>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  wrapper: {
+    paddingVertical: 40,
+    backgroundColor: colors.white,
+    height: '100%',
+    width: '100%',
+  },
+  gap: {
+    paddingVertical: 20,
+  },
+});
 
 export default MyPlanScreen;
