1a6ebc626f5cfbd5b5903b0a6defa1188fa2e7e4
eunseonglee <eunseongofficial@gmail.com>
2021-12-14 22:30:22 +0900

    [#194] feat: 홈 화면 상단 컴포넌트 마크업

--- packages/app/src/screens/HomeScreen.tsx
+++ packages/app/src/screens/HomeScreen.tsx
@@ -1,14 +1,70 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, View, StyleSheet, RefreshControl } from 'react-native';
 
-import { Text } from 'ui';
+import { help, notification } from 'icons';
+import { Button, Text, SvgIcon } from 'ui';
+import { colors } from 'design-token';
+import TimeRemaining from 'components/Home/TimeRemaining';
+import { ScrollView } from 'react-native-gesture-handler';
+import Guide from 'components/Home/Guide';
 
 const MainScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>MainScreen</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <SvgIcon xml={notification} />
+        <View style={{ width: 16 }} />
+        <SvgIcon xml={help} />
+      </View>
+      <ScrollView
+        style={styles.mainContainer}
+        contentContainerStyle={{ backgroundColor: colors.grayBackground }}
+        refreshControl={<RefreshControl refreshing={false} />}>
+        <View style={styles.component}>
+          <TimeRemaining
+            style={styles.timeRemaining}
+            purpose={'출발 시간'}
+            timeRemaining={'2021-12-31 10:00:00'}
+          />
+        </View>
+        <View style={styles.component}>
+          <Guide
+            text={'😂 이러다가 지각러가 된다구요!'}
+            badge={{
+              text: '이달 현황',
+            }}
+          />
+        </View>
+        <View style={{ backgroundColor: colors.grayBackground, height: 12 }} />
+      </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    height: 48,
+  },
+  mainContainer: {
+    height: '100%',
+    backgroundColor: colors.white,
+  },
+  component: {
+    backgroundColor: colors.white,
+    paddingBottom: 20,
+    paddingHorizontal: 20,
+  },
+  timeRemaining: {
+    backgroundColor: colors.white,
+    paddingHorizontal: 12,
+  },
+});
 
 export default MainScreen;
