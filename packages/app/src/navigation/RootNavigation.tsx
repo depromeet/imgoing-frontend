89e7fba9af1defa42bd9cf62753b09f388faaa92
ykss <yukyeongsang@gmail.com>
2021-12-15 21:30:38 +0900

    [#191] feat : 네비게이션 설정

--- packages/app/src/navigation/RootNavigation.tsx
+++ packages/app/src/navigation/RootNavigation.tsx
@@ -4,7 +4,12 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { RootRouterParamList } from 'types/Route';
 import { PlanInputScreen, LoginScreen, AuthLoadingScreen, WebviewScreen } from 'screens';
+import { SvgXml } from 'react-native-svg';
+import { icon_back } from 'icons';
+import { colors } from 'design-token';
 import MainNavigation from './MainNavigation';
+import AccountManageScreen from 'screens/AccountManageScreen';
+import PolicyScreen from 'screens/PolicyScreen';
 
 const Root = createStackNavigator<RootRouterParamList>();
 
@@ -16,6 +21,46 @@ const Stacks = () => {
       <Root.Screen name='PlanEdit' component={PlanInputScreen} />
       <Root.Screen name='Login' component={LoginScreen} />
       <Root.Screen name='Webview' component={WebviewScreen} />
+      <Root.Screen
+        name='AccountManage'
+        component={AccountManageScreen}
+        options={({ navigation }) => ({
+          title: '카카오 연동 계정 관리',
+          headerTitleAlign: 'left',
+          headerLeft: () => {
+            return (
+              <SvgXml
+                xml={icon_back}
+                style={{ marginLeft: 16 }}
+                fill={colors.black}
+                onPressOut={() => {
+                  navigation.goBack();
+                }}
+              />
+            );
+          },
+        })}
+      />
+      <Root.Screen
+        name='Policy'
+        component={PolicyScreen}
+        options={({ navigation }) => ({
+          title: '약관, 개인정보 관리',
+          headerTitleAlign: 'left',
+          headerLeft: () => {
+            return (
+              <SvgXml
+                xml={icon_back}
+                style={{ marginLeft: 16 }}
+                fill={colors.black}
+                onPressOut={() => {
+                  navigation.goBack();
+                }}
+              />
+            );
+          },
+        })}
+      />
     </Root.Navigator>
   );
 };
