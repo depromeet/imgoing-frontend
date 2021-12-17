c7e374ae39aff9a81d877d5128f2c53f9cf5776c
Eungyeol <eungyeole@naver.com>
2021-12-17 22:31:42 +0900

    feat: FixedBottomCTA 컴포넌트 추가

---
+++ packages/ui/src/components/FixedBottomCTA.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { colors } from 'design-token';
+import { StyleSheet, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Button } from '.';
+
+interface Props {
+  children: React.ReactNode;
+  onPress: () => void;
+  text: string;
+  disabled?: boolean;
+  backgroundColor?: string;
+}
+
+export const FixedBottomCTA = (props: Props) => {
+  const { children, text, onPress, disabled, backgroundColor = colors.white } = props;
+  return (
+    <View style={{ flex: 1, backgroundColor }}>
+      <KeyboardAwareScrollView contentContainerStyle={{ paddingBottom: 124 }}>
+        {children}
+      </KeyboardAwareScrollView>
+      <LinearGradient
+        colors={['rgba(255, 255, 255, 0.25)', '#FFFFFF', '#FFFFFF']}
+        style={styles.bottomView}>
+        <View style={styles.buttonView}>
+          <Button onPress={onPress} disabled={disabled}>
+            {text}
+          </Button>
+        </View>
+      </LinearGradient>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  bottomView: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    justifyContent: 'flex-end',
+  },
+  buttonView: {
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+    paddingTop: 52,
+  },
+});
