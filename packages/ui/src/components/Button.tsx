ea4a02ecddb71d76695acd9af107b3c92f7af488
Eungyeol <eungyeole@naver.com>
2021-12-17 22:30:44 +0900

    fix: Button 컴포넌트 오류 수정

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GestureResponderEvent, StyleSheet, TouchableOpacity } from 'react-native';
+import { GestureResponderEvent, StyleSheet, TouchableOpacity, View } from 'react-native';
 import { colors } from 'design-token';
 import { Text } from './Text';
 
@@ -7,18 +7,30 @@ interface ButtonProps {
   children: React.ReactNode;
   onPress: (event: GestureResponderEvent) => void;
   disabled?: boolean;
+  backgroundColor?: string;
+  textColor?: string;
+  leftIcon?: React.ReactNode;
 }
 
 export const Button = (props: ButtonProps) => {
-  const { children, onPress, disabled } = props;
-  const backgroundColor = disabled ? colors.grayMedium : colors.blue;
+  const {
+    children,
+    onPress,
+    disabled,
+    leftIcon,
+    backgroundColor = colors.blue,
+    textColor = colors.white,
+  } = props;
+  const _backgroundColor = disabled ? colors.grayMedium : backgroundColor;
 
   return (
     <TouchableOpacity
-      style={{ ...styles.wrapper, backgroundColor }}
-      activeOpacity={disabled ? 1 : 0.8}
-      onPress={disabled ? onPress : undefined}>
-      <Text color={colors.white} fontType='BOLD_16'>
+      style={{ ...styles.wrapper, backgroundColor: _backgroundColor }}
+      activeOpacity={0.8}
+      disabled={disabled}
+      onPress={onPress}>
+      {leftIcon && <View style={styles.leftIconView}>{leftIcon}</View>}
+      <Text color={textColor} fontType='BOLD_16'>
         {children}
       </Text>
     </TouchableOpacity>
@@ -31,7 +43,11 @@ const styles = StyleSheet.create({
     height: 52,
     borderRadius: 8,
     display: 'flex',
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  leftIconView: {
+    marginRight: 10,
   },
 });
