101d4d5b35dc4ec1f7d583e7f600161f45518b8d
eunseonglee <eunseongofficial@gmail.com>
2021-12-29 03:03:42 +0900

    [#207] feat: useInterval custom hook 추가

---
+++ packages/app/src/hooks/useInterval.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react';
+
+const useInterval = (callback: () => void, delay: number | null) => {
+  const savedCallback = useRef<() => void>(callback);
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    const tick = () => {
+      savedCallback.current();
+    };
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+};
+
+export default useInterval;
