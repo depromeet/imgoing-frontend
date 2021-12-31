18d4c40488fbf2d808a42776a301ee6b7c00dd21
ykss <yukyeongsang@gmail.com>
2022-01-01 02:03:07 +0900

    [#205] feat : 루틴관리 페이지 & 랜딩 화면 추가

--- packages/app/src/screens/RoutineManageScreen.tsx
+++ packages/app/src/screens/RoutineManageScreen.tsx
@@ -2,9 +2,22 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { colors } from 'design-token';
+import RoutineList from 'components/myPlan/Routine/RoutineList';
+import LandingView from 'components/myPlan/Routine/LandingView';
+import { FixedBottomCTA, Stack, Text } from 'ui';
 
 const RoutineManageScreen = () => {
-  return <View style={styles.wrapper}></View>;
+  const routineData = [{}];
+  return (
+    <FixedBottomCTA text='루틴 추가' onPress={() => {}}>
+      <View style={styles.wrapper}>
+        <Stack>
+          <Text fontType='BOLD_24'>자주 사용하는{'\n'}루틴을 등록해 보세요 🙌</Text>
+        </Stack>
+        {routineData.length > 0 ? <RoutineList /> : <LandingView />}
+      </View>
+    </FixedBottomCTA>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -12,6 +25,7 @@ const styles = StyleSheet.create({
     display: 'flex',
     flex: 1,
     backgroundColor: colors.white,
+    paddingTop: '10%',
   },
 });
 
