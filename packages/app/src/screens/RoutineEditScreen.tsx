451dbf64ad2382c106cd69acab91f8210dcd6343
ykss <yukyeongsang@gmail.com>
2022-01-02 01:26:06 +0900

    [#205] feat: RoutineEdit 마크업 일부

--- packages/app/src/screens/RoutineEditScreen.tsx
+++ packages/app/src/screens/RoutineEditScreen.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import TaskItem from 'components/myPlan/Routine/TaskItem';
 import { colors } from 'design-token';
 import { icon_add, task } from 'icons';
@@ -7,21 +8,23 @@ import { SvgXml } from 'react-native-svg';
 import { Divider, FixedBottomCTA, Input, Stack, Text } from 'ui';
 
 const RoutineEditScreen = () => {
-  const taskData = [{}, {}];
+  const taskData = [];
   return (
+    //타이틀이 없거나 준비항목이 0일 경우 버튼 disabled 처리
     <FixedBottomCTA text='저장' onPress={() => {}}>
       <Stack>
         <Text fontType='BOLD_16' style={styles.title}>
           루틴 타이틀
         </Text>
-        <Input placeholder='기존 루틴명' />
+        <Input placeholder='루틴명을 입력하세요' />
       </Stack>
       <Divider />
       <Stack>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
           <View style={{ flex: 5 }}>
-            <Text fontType='BOLD_16'>준비 항목 (4)</Text>
+            <Text fontType='BOLD_16'>준비 항목 ({taskData.length})</Text>
           </View>
+          {/* 히스토리 버튼 눌렀을 때 모달 창 */}
           <Pressable style={{ flex: 1 }} onPress={() => {}}>
             <Text fontType='BOLD_12' color={colors.grayDark}>
               히스토리
@@ -31,6 +34,7 @@ const RoutineEditScreen = () => {
         {taskData.map(() => (
           <TaskItem duration={30} title='브런치 먹기' edit />
         ))}
+        {/* 새로운 준비 항목 눌렀을 때 추가하는 모달창 */}
         <Pressable style={styles.addTaskBtn} onPress={() => {}}>
           <SvgXml xml={icon_add} style={{ marginRight: 10 }} />
           <Text fontType='BOLD_14' color={colors.grayDark}>
--- packages/app/src/screens/RoutineManageScreen.tsx
+++ packages/app/src/screens/RoutineManageScreen.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { colors } from 'design-token';
 import RoutineList from 'components/myPlan/Routine/RoutineList';
 import LandingView from 'components/myPlan/Routine/LandingView';
 import { FixedBottomCTA, Stack, Text } from 'ui';
+import { RootRouterParams } from 'types/Route';
 
 const RoutineManageScreen = () => {
-  const routineData = [{}];
+  const routineData = [];
+  const navigation = useNavigation<RootRouterParams>();
   return (
-    <FixedBottomCTA text='루틴 추가' onPress={() => {}}>
+    <FixedBottomCTA
+      text='루틴 추가'
+      onPress={() => {
+        console.log('pressed');
+        navigation.navigate('RoutineAdd');
+      }}>
       <View style={styles.wrapper}>
         <Stack>
           <Text fontType='BOLD_24'>자주 사용하는{'\n'}루틴을 등록해 보세요 🙌</Text>
