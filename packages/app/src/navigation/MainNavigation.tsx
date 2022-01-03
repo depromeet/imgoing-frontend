df6cbc32ed3863ebdbd663da10470045193f5cea
ykss <yukyeongsang@gmail.com>
2022-01-03 23:37:56 +0900

    feat : 탭바 아이콘 적용

--- packages/app/src/navigation/MainNavigation.tsx
+++ packages/app/src/navigation/MainNavigation.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Svg, { SvgXml } from 'react-native-svg';
 
 import { MainRouterParamList } from 'types/Route';
 import { HomeScreen, SettingsScreen, MyPlanScreen } from 'screens';
+import { Text } from 'ui';
+import { colors } from 'design-token';
+import { icon_home, icon_setting, icon_toys } from 'icons';
 
 const BottomTab = createBottomTabNavigator<MainRouterParamList>();
 
@@ -12,7 +16,16 @@ const MainNavigation = () => {
       <BottomTab.Screen
         name='Home'
         component={HomeScreen}
-        options={{ title: '홈', headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarLabel: ({ focused }) => (
+            <Text fontType='REGULAR_11' color={focused ? colors.black : colors.grayDark}>
+              홈
+            </Text>
+          ),
+          tabBarIcon: ({ focused }) =>
+            focused ? <SvgXml xml={icon_home.active} /> : <SvgXml xml={icon_home.inactive} />,
+        }}
       />
       <BottomTab.Screen
         name='MyPlan'
@@ -20,6 +33,13 @@ const MainNavigation = () => {
         options={{
           title: '약속',
           headerTitleAlign: 'left',
+          tabBarLabel: ({ focused }) => (
+            <Text fontType='REGULAR_11' color={focused ? colors.black : colors.grayDark}>
+              약속
+            </Text>
+          ),
+          tabBarIcon: ({ focused }) =>
+            focused ? <SvgXml xml={icon_toys.active} /> : <SvgXml xml={icon_toys.inactive} />,
         }}
       />
       <BottomTab.Screen
@@ -28,6 +48,13 @@ const MainNavigation = () => {
         options={{
           title: '설정',
           headerTitleAlign: 'left',
+          tabBarLabel: ({ focused }) => (
+            <Text fontType='REGULAR_11' color={focused ? colors.black : colors.grayDark}>
+              설정
+            </Text>
+          ),
+          tabBarIcon: ({ focused }) =>
+            focused ? <SvgXml xml={icon_setting.active} /> : <SvgXml xml={icon_setting.inactive} />,
         }}
       />
     </BottomTab.Navigator>
