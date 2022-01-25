fb74d171ac8521e774ac411e5491404adafada43
eunseonglee <eunseongofficial@gmail.com>
2022-01-25 17:50:40 +0900

    [#213] feat: duration에 맞게 setTimeout 이벤트 설정
    ref를 가진 하위 컴포넌트 강제 업데이트

--- packages/app/src/components/Home/TopContents/index.tsx
+++ packages/app/src/components/Home/TopContents/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { View } from 'react-native';
 
 import { Plan } from 'types';
@@ -6,53 +6,73 @@ import Guide from './Guide';
 import TaskProcess from './TaskProcess';
 import { HomeTopContentsType } from '../type';
 import RemainingTime from './RemainingTime';
+import { differenceInCalendarDays, differenceInSeconds } from 'date-fns';
+import { TimeRemainingRefFunc } from './types';
 
 interface Props {
   plan: Plan;
 }
 
+type ProcessState = {
+  purpose: HomeTopContentsType;
+  duration: 1 | 60;
+  endTime: string;
+};
+
 const TopContents = ({ plan }: Props) => {
-  const [processTime, setProcessTime] = useState<{
-    purpose: HomeTopContentsType;
-    duration: 1 | 60;
-    endTime: string;
-  }>({
+  const timeRemainingRef = useRef<TimeRemainingRefFunc>(null);
+
+  let intervalId = useRef<NodeJS.Timer>();
+  const [processTime, setProcessTime] = useState<ProcessState>({
     purpose: 'oncoming',
-    duration: 1,
+    duration: 60,
     endTime: plan.arrivalAt,
   });
 
   const updateProcess = () => {
     const cur = new Date();
-    if (cur < new Date(plan.startAt)) {
-      setProcessTime({
-        purpose: 'oncoming',
-        duration: 60,
-        endTime: plan.startAt,
-      });
-    } else if (cur < new Date(plan.departureAt)) {
+    const newState: ProcessState = {
+      purpose: 'oncoming',
+      duration: 60,
+      endTime: plan.startAt,
+    };
+    if (new Date(plan.startAt) < cur && cur < new Date(plan.departureAt)) {
       const intendedPlans = plan.tasks.find(
         (task) => new Date(task.startTime) <= new Date() && new Date() < new Date(task.endTime),
       );
-      setProcessTime({
-        purpose: 'process',
-        duration: 1,
-        endTime: intendedPlans?.endTime || plan.departureAt,
-      });
+      newState.purpose = 'process';
+      newState.duration = 1;
+      newState.endTime = intendedPlans?.endTime || plan.departureAt;
     } else if (cur < new Date(plan.arrivalAt)) {
-      setProcessTime({
-        purpose: 'toArrival',
-        duration: 60,
-        endTime: plan.arrivalAt,
-      });
+      newState.purpose = 'toArrival';
+      newState.endTime = plan.arrivalAt;
     }
+
+    setProcessTime(newState);
+
+    const remainingSeconds = differenceInSeconds(new Date(newState.endTime), new Date());
+    const diffDays = differenceInCalendarDays(new Date(newState.endTime), new Date());
+    if (diffDays >= 2) return;
+
+    if (intervalId.current) {
+      clearInterval(intervalId.current);
+    }
+    intervalId.current = setInterval(() => {
+      // update component
+      timeRemainingRef.current?.forceUpdate();
+    }, newState.duration * 1000);
+
+    setTimeout(() => {
+      intervalId.current && clearInterval(intervalId.current);
+      updateProcess();
+    }, remainingSeconds * 1000);
   };
 
   useEffect(updateProcess, []);
 
   return (
     <View>
-      <RemainingTime process={processTime} updateProcess={updateProcess} />
+      <RemainingTime process={processTime} ref={timeRemainingRef} />
       {processTime.purpose === 'process' ? (
         <TaskProcess tasks={plan.tasks} />
       ) : (
