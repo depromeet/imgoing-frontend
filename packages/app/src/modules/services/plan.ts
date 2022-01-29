8e5a90fa6cec4a3a6a09f234858cb9b049286f2d
eunseonglee <eunseongofficial@gmail.com>
2022-01-29 18:35:49 +0900

    [#213] feat: data에서 지난 일정이 있다면 제외

--- packages/app/src/modules/services/plan.ts
+++ packages/app/src/modules/services/plan.ts
@@ -13,44 +13,46 @@ export const planApi = createApi({
     getPlans: builder.query<Plan[], void>({
       query: () => ({ url: '/plans' }),
       transformResponse: (response: { data: ResPlan[] }) =>
-        response.data.map((plan) => ({
-          id: plan.id,
-          name: plan.name,
-          arrivalAt: plan.arrivalAt,
-          arrival: {
-            name: plan.arrivalName,
-            lat: plan.arrivalLat,
-            lng: plan.arrivalLng,
-          },
-          departureAt: format(
-            add(new Date(plan.startAt), {
-              minutes: plan.task.reduce((acc, cur) => acc + cur.time, 0),
-            }),
-            'yyyy-MM-dd HH:mm:ss',
-          ),
-          departure: {
-            name: plan.departureName,
-            lat: plan.departureLat,
-            lng: plan.departureLng,
-          },
-          belongings: plan.belongings,
-          memo: plan.memo,
-          tasks: plan.task.reduce((tasks, cur, idx): Task[] => {
-            const startTime =
-              tasks.length === 0
-                ? plan.startAt
-                : format(
-                    add(new Date(tasks[idx - 1].startTime), { minutes: tasks[idx - 1].time }),
-                    'yyyy-MM-dd HH:mm:ss',
-                  );
-            const endTime = format(
-              add(new Date(startTime), { minutes: cur.time }),
+        response.data
+          .filter((plan) => new Date() < new Date(plan.arrivalAt))
+          .map((plan) => ({
+            id: plan.id,
+            name: plan.name,
+            arrivalAt: plan.arrivalAt,
+            arrival: {
+              name: plan.arrivalName,
+              lat: plan.arrivalLat,
+              lng: plan.arrivalLng,
+            },
+            departureAt: format(
+              add(new Date(plan.startAt), {
+                minutes: plan.task.reduce((acc, cur) => acc + cur.time, 0),
+              }),
               'yyyy-MM-dd HH:mm:ss',
-            );
-            return [...tasks, { ...cur, startTime, endTime }];
-          }, [] as Task[]),
-          startAt: plan.startAt,
-        })),
+            ),
+            departure: {
+              name: plan.departureName,
+              lat: plan.departureLat,
+              lng: plan.departureLng,
+            },
+            belongings: plan.belongings,
+            memo: plan.memo,
+            tasks: plan.task.reduce((tasks, cur, idx): Task[] => {
+              const startTime =
+                tasks.length === 0
+                  ? plan.startAt
+                  : format(
+                      add(new Date(tasks[idx - 1].startTime), { minutes: tasks[idx - 1].time }),
+                      'yyyy-MM-dd HH:mm:ss',
+                    );
+              const endTime = format(
+                add(new Date(startTime), { minutes: cur.time }),
+                'yyyy-MM-dd HH:mm:ss',
+              );
+              return [...tasks, { ...cur, startTime, endTime }];
+            }, [] as Task[]),
+            startAt: plan.startAt,
+          })),
     }),
   }),
 });
