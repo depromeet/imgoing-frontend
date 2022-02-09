1451f96ada5f4252e00f671a5a7097ef00ce3baa
ykss <yukyeongsang@gmail.com>
2022-02-10 01:07:12 +0900

    fix : 탭바 코드 삭제

--- packages/app/src/navigation/MainNavigation.tsx
+++ packages/app/src/navigation/MainNavigation.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Svg, { SvgXml } from 'react-native-svg';
 
 import { MainRouterParamList } from 'types/Route';
 import { HomeScreen, SettingsScreen, MyPlanScreen } from 'screens';
-import { Text } from 'ui';
-import { colors } from 'design-token';
-import { icon_home, icon_setting, icon_toys } from 'icons';
 
 const BottomTab = createBottomTabNavigator<MainRouterParamList>();
 
@@ -16,46 +12,17 @@ const MainNavigation = () => {
       <BottomTab.Screen
         name='Home'
         component={HomeScreen}
-        options={{
-          headerShown: false,
-          tabBarLabel: ({ focused }) => (
-            <Text fontType='REGULAR_11' color={focused ? colors.black : colors.grayDark}>
-              홈
-            </Text>
-          ),
-          tabBarIcon: ({ focused }) =>
-            focused ? <SvgXml xml={icon_home.active} /> : <SvgXml xml={icon_home.inactive} />,
-        }}
+        options={{ title: '홈', headerShown: false }}
       />
       <BottomTab.Screen
         name='MyPlan'
         component={MyPlanScreen}
-        options={{
-          title: '약속',
-          headerTitleAlign: 'left',
-          tabBarLabel: ({ focused }) => (
-            <Text fontType='REGULAR_11' color={focused ? colors.black : colors.grayDark}>
-              약속
-            </Text>
-          ),
-          tabBarIcon: ({ focused }) =>
-            focused ? <SvgXml xml={icon_toys.active} /> : <SvgXml xml={icon_toys.inactive} />,
-        }}
+        options={{ title: '약속', headerShown: false }}
       />
       <BottomTab.Screen
         name='Settings'
         component={SettingsScreen}
-        options={{
-          title: '설정',
-          headerTitleAlign: 'left',
-          tabBarLabel: ({ focused }) => (
-            <Text fontType='REGULAR_11' color={focused ? colors.black : colors.grayDark}>
-              설정
-            </Text>
-          ),
-          tabBarIcon: ({ focused }) =>
-            focused ? <SvgXml xml={icon_setting.active} /> : <SvgXml xml={icon_setting.inactive} />,
-        }}
+        options={{ title: '설정', headerShown: false }}
       />
     </BottomTab.Navigator>
   );
